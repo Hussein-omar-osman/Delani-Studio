e1f298d49d8b3b3a825bcac1cbcb18a5eaa633d4
Huseein Omar <husseinomar6190@gmail.com>
2021-11-22 03:01:20 +0300

    adding a fuction for form submit

--- js/script.js
+++ js/script.js
@@ -14,3 +14,23 @@ $(document).ready(function() {
    $("#product-management-showing").toggle();
    $(".hide-product-management").toggle();
  })
+
+   //Insert white box upo hover
+   $(".Hover").hover(function(){
+    $(this).animate({opacity:'1'});
+  },
+  function(){
+    $(this).animate({opacity:'0'});
+  })
+$('.submit').click(function () {
+        var Name = $('#mce-FNAME').val();
+        var Email = $('#mce-EMAIL').val();
+        var Message = $('#mce-MESSAGE');
+        var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
+        if (Name == '' || Email == '' || Message == '') {
+            alert('Please make sure you have filled in the form correctly!');
+        } else {
+            alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+        }
+  });
+});
