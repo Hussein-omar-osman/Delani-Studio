704a2ff05648c8cacbb78ba9bfa720ec23c2ad47
Huseein Omar <husseinomar6190@gmail.com>
2021-11-24 10:23:11 +0300

    small changes to submit button

--- js/script.js
+++ js/script.js
@@ -16,16 +16,16 @@ $(document).ready(function() {
  })
 
    //Insert white box upo hover
-   $(".Hover").hover(function(){
+  $(".Hover").hover(function(){
     $(this).animate({opacity:'1'});
   },
   function(){
     $(this).animate({opacity:'0'});
   })
 $('.submit').click(function () {
-        var Name = $('#mce-FNAME').val();
-        var Email = $('#mce-EMAIL').val();
-        var Message = $('#mce-MESSAGE');
+        var Name = $('#user-name').val();
+        var Email = $('#user-email').val();
+        var Message = $('#user-comment');
         var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
         if (Name == '' || Email == '' || Message == '') {
             alert('Please make sure you have filled in the form correctly!');
